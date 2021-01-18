b9234b647ef8ddb857604a2b7c97c23f28d0894b
HangVo123 <48342625+HangVo123@users.noreply.github.com>
2021-01-19 10:04:37 +1100

    arrays

---
+++ arrays/todo.js
@@ -0,0 +1,5 @@
+const todos = ['Order rice cake', 'Carpet steam cleaning', 'Walk the dog', 'Lesmill time']
+
+console.log(`You have ${todos.length} things to do in your list`)
+console.log(`Todo: ${todos[0]}`)
+console.log(`Todo: ${todos[todos.length - 1]}`)
