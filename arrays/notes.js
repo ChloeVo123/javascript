0be7ee14e0dc1b684b56d6873f5975e2ba861b20
HangVo123 <48342625+HangVo123@users.noreply.github.com>
2021-01-19 10:47:27 +1100

    array manipulating

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,4 +1,18 @@
 const notes = ['Note 1', 'Note 2', 'Note 3']
 
+// //Remove item from the end out of the array
+// console.log(notes.pop()) 
+// //Add item to the end
+// notes.push("My new todo")
+// //Remove the first item from the array 
+// console.log(notes.shift())
+// console.log(notes.unshift('My first note'))
+
+// notes.splice(1, 1, 'This is the new second item')
+
+notes[2] = 'This is the new note 3'
+
 console.log(notes.length)
-console.log(notes[notes.length - 1])
+// console.log(notes[notes.length - 1])
+
+console.log(notes)
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,5 +1,12 @@
 const todos = ['Order rice cake', 'Carpet steam cleaning', 'Walk the dog', 'Lesmill time']
 
-console.log(`You have ${todos.length} things to do in your list`)
-console.log(`Todo: ${todos[0]}`)
-console.log(`Todo: ${todos[todos.length - 1]}`)
+//Delete the third item
+console.log(todos.splice(2, 1))
+//Add new item onto the end
+console.log(todos.push('Buy coffee'))
+//Remove first item from the list
+console.log(todos.shift())
+//console.log(`You have ${todos.length} things to do in your list`)
+// console.log(`Todo: ${todos[0]}`)
+// console.log(`Todo: ${todos[todos.length - 1]}`)
+console.log(todos)
