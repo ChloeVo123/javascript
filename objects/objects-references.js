bcfc7acc7b7e536038ce10455a9cecbf32ea6f69
HangVo123 <48342625+HangVo123@users.noreply.github.com>
2021-01-18 18:24:51 +1100

    object references

---
+++ objects/objects-references.js
@@ -0,0 +1,14 @@
+let myAccount = {
+    name: 'Chloe',
+    expenses: 0,
+    income: 0
+}
+
+let addExpense = function(account, amount) {
+    account = {}
+    //account.expenses = account.expenses + amount
+    console.log(account)
+}
+
+addExpense(myAccount, 2)
+console.log(myAccount)
