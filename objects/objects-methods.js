6016ec0d9015537145056f6d43674aac7517fd79
HangVo123 <48342625+HangVo123@users.noreply.github.com>
2021-01-18 23:11:56 +1100

    objects methods

---
+++ objects/objects-methods.js
@@ -0,0 +1,26 @@
+// a method is just a object property whose value is a function 
+
+let restaurant = {
+    name: "Basil Town",
+    guestCapacity: 75,
+    guestCount: 0,
+    checkAvalability: function(partySize) {
+        let seatsLeft = this.guestCapacity - this.guestCount
+        return partySize <= seatsLeft
+    },
+    seatsParty: function(partySize) {
+        this.guestCount = this.guestCount + partySize
+    },
+    removeParty: function(partySize) {
+        this.guestCount = this.guestCount - partySize
+    }
+}
+
+//seatsParty
+
+//removeParty
+
+restaurant.seatsParty(72)
+console.log(restaurant.checkAvalability(4))
+restaurant.removeParty(5)
+console.log(restaurant.checkAvalability(4))
--- objects/objects-references.js
+++ objects/objects-references.js
@@ -5,10 +5,32 @@ let myAccount = {
 }
 
 let addExpense = function(account, amount) {
-    account = {}
-    //account.expenses = account.expenses + amount
+    account.expenses = account.expenses + amount
     console.log(account)
 }
 
-addExpense(myAccount, 2)
-console.log(myAccount)
+// add income
+let addIncome = function(account, amount ) {
+    account.income = account.income + amount
+}
+//reset account
+let resetAccount = function(account) {
+    account.expenses = 0
+    account.income = 0
+}
+//get account summary 
+let getAccountSummary = function(account) {
+    let balance = account.income -account.expenses
+    return `Account for ${account.name} has $${balance}. $${account.income} in income. $${account.expenses} in expenses`
+}
+
+// addExpense(myAccount, 2)
+// console.log(myAccount)
+
+addIncome(myAccount, 2000)
+addExpense(myAccount, 2.50)
+addExpense(myAccount, 160)
+console.log(getAccountSummary(myAccount))
+
+
+//reference types: array, function, object 
