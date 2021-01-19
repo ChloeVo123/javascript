3da0d00c3cc2e80d01cfa3c74d9e765a9d200725
HangVo123 <48342625+HangVo123@users.noreply.github.com>
2021-01-19 13:52:03 +1100

    DOM Manipulation

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,16 @@
-console.log('This is from a different file')
+// DOM - DOcument (HTML doc) Object (JS object) Model.
+// The document is modeled using a javascript object and that object is called document
+
+
+//Query and Remove
+// const p = document.querySelector('p')
+// p.remove()
+
+//Querry all and remove
+const ps = document.querySelectorAll('p')
+
+ps.forEach( function (p) {
+    p.textContent = "***********"
+    //console.log(p.textContent)
+    //p.remove()
+})
